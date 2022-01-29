6361c9d0469cdfa11372df9dd5e2c6f21e2ccfc4
prasagungg <agungprasetio878@gmail.com>
2022-01-29 20:16:55 +0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import AboutPage from "./component/pages/AboutPage";
 
 import Header from "./component/Header";
 import FeedbackList from "./component/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./component/FeedbackStats";
 import FeedbackForm from "./component/FeedbackForm";
+import AboutIconLink from "./component/AboutIconLink";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -22,14 +26,29 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedBack} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedBack}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/component/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "../shared/Card";
+import { Link } from "react-router-dom";
+
+function AboutPage() {
+  return (
+    <Card>
+      <h1>About this Project</h1>
+      <p>This is React app to leave feedback for a product or service</p>
+      <p>Version: 1.0.0</p>
+      <p>
+        <Link to="/">Back To Home</Link>
+      </p>
+    </Card>
+  );
+}
+
+export default AboutPage;
